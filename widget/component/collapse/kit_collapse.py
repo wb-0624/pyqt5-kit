8933b98add19e664a70660afff33b9596faf6b2b
wubin <2531363057@qq.com>
2024-01-15 21:33:46 +0800

    update：KitCollapse

--- widget/component/collapse/kit_collapse.py
+++ widget/component/collapse/kit_collapse.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt, pyqtSignal
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout
 
 from app_config import Icons
 from ..icon import KitIcon
@@ -7,132 +7,130 @@ from ..icon import KitIcon
 
 class KitCollapse(QWidget):
 
-    def __init__(self, title: str, icon: str = None, parent=None):
+    def __init__(self, title: str, content: QWidget, parent=None):
         super(KitCollapse, self).__init__(parent=parent)
 
-        self.title = title
-        self.icon = icon
+        self._item = KitCollapseItem(title, self)
+        self._content = KitCollapseContent(content, self)
         self._show_content = False
-
-        self._ani_show = None
-        self._ani_hide = None
-        self._ani_start_value = None
-        self._ani_end_value = None
 
         self.__init_widget()
         self.__init_slot()
         self.__init_qss()
 
     def __init_widget(self):
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
+        self.layout = QVBoxLayout(self)
         self.layout.setContentsMargins(0, 0, 0, 0)
-
-        self.collapse_item = KitCollapseItem(self.title, self.icon, self)
-        self.collapse_content = KitCollapseContent()
-        self.collapse_content.hide()
-
-        self.layout.addWidget(self.collapse_item)
-        self.layout.addWidget(self.collapse_content)
+        self.layout.setSpacing(0)
+        self.layout.addWidget(self._item)
+        self.layout.addWidget(self._content)
+        self.setLayout(self.layout)
+        self._fresh_content()
 
     def __init_slot(self):
-        self.collapse_item.clicked.connect(self._change_show_content)
+        self._item.clicked.connect(self._change_state)
 
     def __init_qss(self):
         self.setAttribute(Qt.WA_StyledBackground, True)
 
-    def _change_show_content(self):
-        self._show_content = False if self._show_content else True
-        self.collapse_item.setState(self._show_content)
-        self.collapse_content.setVisible(self._show_content)
+    def _change_state(self):
+        self._show_content = not self._show_content
+        self._fresh_content()
 
-    def setTitle(self, title):
-        self.collapse_item.setTitle(title)
+    def _fresh_content(self):
+        self._item.setState(self._show_content)
+        self._content.setVisible(self._show_content)
+
+    def setTitle(self, title: str):
+        self._item.setTitle(title)
+
+    def setTitleIndicatorIcon(self, content_hide, content_show):
+        self._item.setIndicatorIcon(content_hide, content_show)
+
+    def setTitleAlignment(self, alignment: Qt.Alignment):
+        self._item.setAlignment(alignment)
 
     def setIcon(self, icon):
-        self.collapse_item.setIcon(icon)
+        self._item.setIcon(icon)
 
-    def setContent(self, content: QWidget):
-        self.collapse_content.setContent(content)
-        self.collapse_content.adjustSize()
+    def setContent(self, widget: QWidget):
+        self._content.setContent(widget)
 
 
 class KitCollapseItem(QWidget):
     clicked = pyqtSignal()
 
-    def __init__(self, title: str, icon: str = None, parent=None):
+    def __init__(self, title: str, parent=None):
         super(KitCollapseItem, self).__init__(parent=parent)
-        self.icon = KitIcon(icon)
-        self.title = QLabel(title)
-
-        self.right_arrow = Icons.md_chevron_right
-        self.down_arrow = Icons.md_expand_more
-
-        self.indicator = KitIcon(self.right_arrow)
+        self._title = QLabel(title, self)
+        self._icon = KitIcon(None)
         self._state = False
+        self._indicator_icon_show = Icons.md_expand_more
+        self._indicator_icon_hide = Icons.md_chevron_right
+        self._indicator_icon = KitIcon(self._indicator_icon_hide)
 
         self.__init_widget()
         self.__init_slot()
         self.__init_qss()
 
     def __init_widget(self):
-        self.layout = QHBoxLayout()
+        self.layout = QHBoxLayout(self)
         self.setLayout(self.layout)
-
-        self.setContentsMargins(4, 4, 4, 4)
-        self.setFixedHeight(40)
-
-        self.layout.addWidget(self.icon)
-        self.layout.addWidget(self.title)
-        self.layout.addStretch(1)
-        self.layout.addWidget(self.indicator)
+        self.layout.addWidget(self._icon)
+        self.layout.addWidget(self._title, stretch=1)
+        self.layout.addWidget(self._indicator_icon)
 
     def __init_slot(self):
-        pass
+        self.clicked.connect(lambda: self._change_show_content())
+
+    def setState(self, state: bool):
+        self._state = state
+        self._fresh_indicator_icon()
+
+    def _change_show_content(self):
+        self._state = not self._state
+        self._fresh_indicator_icon()
+
+    def _fresh_indicator_icon(self):
+        if self._state:
+            self._indicator_icon.setIcon(self._indicator_icon_show)
+        else:
+            self._indicator_icon.setIcon(self._indicator_icon_hide)
 
     def __init_qss(self):
         self.setAttribute(Qt.WA_StyledBackground, True)
 
-    def setTitle(self, title):
-        self.title.setText(title)
+    def setTitle(self, title: str):
+        self._title.setText(title)
 
     def setIcon(self, icon):
-        self.icon.setIcon(icon)
+        self._icon.setIcon(icon)
 
-    def mousePressEvent(self, a0) -> None:
+    def setAlignment(self, alignment: Qt.Alignment):
+        self._title.setAlignment(alignment)
+
+    def setIndicatorIcon(self, content_hide, content_show):
+        self._indicator_icon_hide = content_hide
+        self._indicator_icon_show = content_show
+        self._fresh_indicator_icon()
+
+    def mousePressEvent(self, ev) -> None:
         self.clicked.emit()
-        return super().mousePressEvent(a0)
-
-    def _change_state(self):
-        self._state = False if self._state else True
-        self.indicator.setIcon(self.down_arrow if self._state else self.right_arrow)
-
-    def setState(self, state):
-        self._state = state
-        self.indicator.setIcon(self.down_arrow if self._state else self.right_arrow)
-
-    def enterEvent(self, a0) -> None:
-        self.setCursor(Qt.PointingHandCursor)
-        return super().enterEvent(a0)
-
-    def leaveEvent(self, a0) -> None:
-        self.setCursor(Qt.ArrowCursor)
-        return super().leaveEvent(a0)
 
 
 class KitCollapseContent(QWidget):
 
-    def __init__(self, parent=None):
+    def __init__(self, content: QWidget, parent=None):
         super(KitCollapseContent, self).__init__(parent=parent)
-
-        self.content = None
+        self._content = content
 
         self.__init_widget()
         self.__init_slot()
         self.__init_qss()
 
     def __init_widget(self):
-        self.layout = QVBoxLayout()
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self._content)
         self.setLayout(self.layout)
 
     def __init_slot(self):
@@ -141,6 +139,8 @@ class KitCollapseContent(QWidget):
     def __init_qss(self):
         self.setAttribute(Qt.WA_StyledBackground, True)
 
-    def setContent(self, content: QWidget):
-        self.content = content
-        self.layout.addWidget(content)
+    def setContent(self, widget: QWidget):
+        self.layout.removeWidget(self._content)
+        self._content.deleteLater()
+        self._content = widget
+        self.layout.addWidget(self._content)
